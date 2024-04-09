6e42b40f132bb6b3f82f14de2ade020354d9a0a1
KingGodAGENT <166516011+KingGodAGENT@users.noreply.github.com>
2024-04-09 21:41:23 +0900

    Add files via upload

---
+++ Day01/input.py
@@ -0,0 +1,17 @@
+#print : 글 나오게하는 기능
+#input : 글을 입력하는 기능
+
+# drama = input("당신의 최애 드라마는?")
+# print(f"당신의 최애 드라마는 {drama}이군요!")
+#
+# name = input("당신의 이름은?")
+# print(f"당신의 이름은 {name}이군요!")
+
+# name = input("당신의 이름은?")
+# age = input("당신의 나이는?")
+# color = input("당신이 좋아하는 색은?")
+# print(f"제 이름은 {name}, 나이는 {age}살이고 좋아하는색은 {color}입니다!")
+
+# date = input("오늘은 몇년도 몇월 며칠입니까?")
+# scadule = input("계획하신 일정은?")
+# print(f"오늘은 {date}, 계획하신 일정은 {scadule}입니다.")
---
+++ Day01/variable.py
@@ -0,0 +1,15 @@
+# 변수(variable)
+# 오른쪽 데이터를 왼쪽의 변수에 넣는다.
+name = "이우성"
+major = "컴퓨터공학과"
+print(name)
+print(major)
+fruit = "사과, 방울토마토"
+print(fruit)
+
+# f문자열 문법
+print(f"저는 {name} 입니다")
+# 저의 이름은 ~ 저의 전공은 ~이고 제가 좋아하는 과일은 ~ 입니다.
+print(f"저의 이름은 {name}, 저의 전공은 {major}이고 제가 좋아하는 과일은 {fruit}입니다.")
+
+# 변수명 문법 규칙
