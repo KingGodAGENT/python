95a35c4d68c3140324e6b90ce28d2caaf0a033c8
KingGodAGENT <166516011+KingGodAGENT@users.noreply.github.com>
2024-04-11 21:51:58 +0900

    Add files via upload

---
+++ Day02/datatype.py
@@ -0,0 +1,46 @@
+# 데이터 타입
+# 변수 : RAM에 저장하는 공간
+# 2 + 3 = 5 인간 : 연산 + 기억 [뇌]
+# 2 + 3 = 5 컴퓨터 : 연산[CPU] + 기억[RAM] (폰노이만 구조)
+# a = 2 + 3 [숫자]
+# a = "2 + 3" [문자]
+
+# 데이터 타입 종류
+
+# a1 = 1 #정수형 타입
+# a2 = 3.14 #실수형 타입
+# a3 = "3.14" #문자형 타입
+# a4 = True #불리언형 타입 [Yes or No]
+# a5 = False #불리언형 타입 [Yes or No]
+# a6 = None #None형 타입 [없음]
+
+# 정수형, 실수형 타입
+# 사칙연산이 적용됨
+# b = 2 + 3.14
+# 문자형 타입
+# 연결 연산
+# c = "300" + "100"
+# 반복 연산
+# c1 = "300" * 5
+# print(c1)
+
+# print : 출력 기능
+# input : 입력 기능
+# type : 타입을 알려주는 기능
+# print(type(1))
+# print(type("1"))
+# print(type(True))
+# print(type(None))
+
+# type-casting[타입 변환]
+# 정수화 : int()
+d = int("100")
+d1 = int(3.14)
+print(d1)
+
+# 문자화 : str()
+f = str(True)
+print(f)
+
+# 불리언화 해주는 기능
+bool("") True[나머지전부다] or False[0,"",None]
